94d7027b4743d722813acd14329d44e117af98e9
Eric Duran <mediaboxeadl@outlook.com>
2015-03-23 21:14:29 +0000

    Changed movie processor to use Movie Insider's Web page for movies coming out this week as source of new movie info

--- processors/movies.js
+++ processors/movies.js
@@ -1,44 +1,13 @@
 "use strict";
 
-function requestMovieCalendar(request, startDate, endDate) {
-    var today = new Date(),
-        thisYear = today.getFullYear(),
-        thisMonth = today.getMonth() + 1,
-        nextMonth = thisMonth + 1,
-        day,
-        startMin,
-        startMax,
-        timePart = 'T00:00:00',
-        movieCalendar = 'https://www.google.com/calendar/feeds/pfutdblf1gi8jmfsvroh76f6jg%40group.calendar.google.com/public/basic?start-min={0}&start-max={1}&orderby=starttime&sortorder=a';
-
-    nextMonth = nextMonth === 13 ? 12 : nextMonth;
-    day = nextMonth === 12 ? '31' : '01';
-    thisMonth = thisMonth < 10 ? '0' + thisMonth : thisMonth;
-    nextMonth = nextMonth < 10 ? '0' + nextMonth : nextMonth;
-
-    if (startDate) {
-        startMin = startDate + timePart;
-    } else {
-        startMin = '{0}-{1}-01'.format(thisYear, thisMonth) + timePart;
-    }
-    if (endDate) {
-        startMax = endDate + timePart;
-    } else {
-        startMax = '{0}-{1}-{2}'.format(thisYear, nextMonth, day) + timePart;
-    }
-    movieCalendar = movieCalendar.format(startMin, startMax);
-
-    return request(movieCalendar);
-}
-
-var getMovieDocument = function (Movie, movieInfo, firstShowingUrl) {
+var getMovieDocument = function (Movie, movieInfo) {
     var none = 'N/A',
         noPoster = 'http://www.sourcecreative.net/wp-content/uploads/2013/11/values-are-not-a-poster.jpg',
         previousYear = new Date().getFullYear() - 1,
         movie,
         runtimeInMinutes,
         runtimeInMinutesEndIndex;
-    runtimeInMinutes = movieInfo.Runtime === none ? null : movieInfo.Runtime;
+    runtimeInMinutes = (movieInfo.Runtime === none || !movieInfo.Runtime) ? null : movieInfo.Runtime;
     if (runtimeInMinutes !== null) {
         runtimeInMinutesEndIndex = runtimeInMinutes.indexOf(' ');
         if (runtimeInMinutesEndIndex > 0) {
@@ -63,8 +32,7 @@ var getMovieDocument = function (Movie, movieInfo, firstShowingUrl) {
         country: movieInfo.Country,
         awards: movieInfo.Awards,
         poster: movieInfo.Poster === none ? noPoster : movieInfo.Poster,
-        imdbId: movieInfo.imdbID,
-        firstShowingUrl: firstShowingUrl
+        imdbId: movieInfo.imdbID
     });
     if (isNaN(movie.year)) {
         movie.year = null;
@@ -78,34 +46,38 @@ var getMovieDocument = function (Movie, movieInfo, firstShowingUrl) {
     return movie;
 };
 
-function processMovieRequests(res, Promise, movies, Movie, movieTitles, firstShowingUrls, movieRequestResults) {
+function processMovieRequests(res, Promise, movies, Movie, movieTitles, movieRequestResults) {
     var findMoviePromises = movieRequestResults.map(function (movieRequestResult, index) {
         var errorMessage,
             resultValue,
             movieResponse,
             movieResponseBody,
-            movieInfo;
+            movieInfo,
+            rejectionReason;
 
         if (movieRequestResult.isFulfilled()) {
             resultValue = movieRequestResult.value();
             movieResponse = resultValue[0];
             movieResponseBody = resultValue[1];
             if (movieResponse.statusCode !== 200) {
-                errorMessage = 'Cannot retrieve movie information for "{0}" via GET request, got status code: {0}\r\n'.format(movieTitles[index], movieResponse.statusCode);
+                errorMessage = 'Cannot retrieve movie information for "{0}" via GET request, got status code: {1}\r\n'.format(movieTitles[index], movieResponse.statusCode);
                 res.write(errorMessage);
                 return Promise.reject(errorMessage);
             }
             movieInfo = JSON.parse(movieResponseBody);
-            if (movieInfo.Response) {
-                movies[index] = getMovieDocument(Movie, movieInfo, firstShowingUrls[index]);
+            if (movieInfo.Response === "True") {
+                movies[index] = getMovieDocument(Movie, movieInfo);
                 return Movie.findOne({ imdbId: movies[index].imdbId }).exec();
             }
             errorMessage = 'Movie information for "{0}" was not found at the Open Movie Database (OMDb)\r\n'.format(movieTitles[index]);
             res.write(errorMessage);
             return Promise.reject({ handled: true });
         }
-        errorMessage = 'Cannot retrieve movie information for "{0}" via GET request, got: {1}\r\n'.format(movieTitles[index], movieRequestResult.reason());
-        res.write(errorMessage);
+        rejectionReason = movieRequestResult.reason();
+        if (rejectionReason.handled === undefined) {
+            errorMessage = 'Error while processing request for "{0}": }\r\n'.format(movieTitles[index]);
+            res.write(errorMessage);
+        }
         return Promise.reject({ handled: true });
     });
     return findMoviePromises;
@@ -136,52 +108,117 @@ function saveMovies(res, Promise, movies, Movie, findMoviePromiseResults) {
     return createMoviePromises;
 }
 
+function processInsiderMovieRequests(res, movieTitles, insiderMoviesUrls, movieRequestResults) {
+    var findMoviePromises = movieRequestResults.map(function (movieRequestResult, index) {
+        var errorMessage,
+            resultValue,
+            movieResponse,
+            movieResponseBody,
+            imdbIdUrl = 'http://www.imdb.com/title/tt',
+            imdbUrlStartIndex,
+            imdbIdStartIndex,
+            nationwideReleaseScopeTag = '&nbsp;&nbsp;&nbsp;<span>',
+            releaseScopeStartIndex,
+            releaseScopeEndIndex,
+            releaseScope;
+
+        if (movieRequestResult.isFulfilled()) {
+            resultValue = movieRequestResult.value();
+            movieResponse = resultValue[0];
+            movieResponseBody = resultValue[1];
+            if (movieResponse.statusCode !== 200) {
+                errorMessage = 'Cannot retrieve movie information from Movie Insider site for "{0}" via GET request, got status code: {1}\r\n'.format(movieTitles[index], movieResponse.statusCode);
+                res.write(errorMessage);
+                return null;
+            }
+            imdbUrlStartIndex = movieResponseBody.indexOf(imdbIdUrl);
+            if (imdbUrlStartIndex < 0) {
+                errorMessage = 'Cannot find ID in IMDb URL for "{0}" in content of Movie Insider Web page {1}\r\n'.format(movieTitles[index], insiderMoviesUrls[index]);
+                res.write(errorMessage);
+                return null;
+            }
+            releaseScopeStartIndex = movieResponseBody.indexOf(nationwideReleaseScopeTag);
+            if (releaseScopeStartIndex < 0) {
+                errorMessage = 'Cannot find info about scope of release for "{0}" in content of Movie Insider Web page {1}\r\n'.format(movieTitles[index], insiderMoviesUrls[index]);
+                res.write(errorMessage);
+                return null;
+            }
+            releaseScopeEndIndex = movieResponseBody.indexOf('&nbsp;', releaseScopeStartIndex + nationwideReleaseScopeTag.length);
+            releaseScope = movieResponseBody.substring(releaseScopeStartIndex + nationwideReleaseScopeTag.length, releaseScopeEndIndex);
+            if (releaseScope !== 'Nationwide' || releaseScope !== 'Limited') {
+                errorMessage = 'Stopping processing of movie "{0}" since it was not released nationwide per {1}\r\n'.format(movieTitles[index], insiderMoviesUrls[index]);
+                res.write(errorMessage);
+                return null;
+            }
+            imdbIdStartIndex = imdbUrlStartIndex + imdbIdUrl.length - 2;
+            return movieResponseBody.substring(imdbIdStartIndex, movieResponseBody.indexOf('/', imdbIdStartIndex));
+        }
+        errorMessage = 'Cannot retrieve movie information for "{0}" via GET request, got: {1}\r\n'.format(movieTitles[index], movieRequestResult.reason());
+        res.write(errorMessage);
+        return null;
+    });
+    return findMoviePromises;
+}
+
 function processMovieCalendar(res, request, Promise, calendarResponse, calendarBody) {
     var xpath = require('xpath'),
         Dom = require('xmldom').DOMParser,
         Movie = require('../schemas/movie.js'),
-        movieInfo = 'http://www.omdbapi.com/?t={0}&type=movie&plot=full&r=json',
+        movieInfo = 'http://www.omdbapi.com/?i={0}&plot=full&r=json',
         movies = [],
+        moviesStartIndex,
+        moviesEndIndex,
+        moviesContent,
         doc,
-        select,
         nodes,
         movieTitles,
-        firstShowingUrls,
-        movieRequests;
+        insiderMoviesUrls,
+        insiderMoviesRequests;
 
     if (calendarResponse.statusCode !== 200) {
-        res.write("Cannot retrieve movie calendar via GET request, got status code: " + calendarResponse.statusCode);
+        res.write("Cannot retrieve movies via GET request, got status code: " + calendarResponse.statusCode);
         res.end();
         return;
     }
-
-    res.write("Retrieved movie calendar via GET request\r\n");
-    doc = new Dom().parseFromString(calendarBody);
-    select = xpath.useNamespaces({"atom": "http://www.w3.org/2005/Atom"});
-    nodes = select('//atom:feed/atom:entry/atom:title/text()', doc);
+    res.write("Retrieved movies via GET request\r\n");
+    moviesStartIndex = calendarBody.indexOf('<div style="clear:both;">');
+    if (moviesStartIndex < 0) {
+        res.write('Cannot find start of movies information');
+        res.end();
+        return;
+    }
+    moviesEndIndex = calendarBody.indexOf('</ul><br /><ul class="selectsub">', moviesStartIndex);
+    if (moviesEndIndex < moviesStartIndex) {
+        res.write('Cannot find end of movies information');
+        res.end();
+        return;
+    }
+    moviesContent = '<?xml version="1.0" encoding="windows-1252"?><movies>';
+    moviesContent += calendarBody.substring(moviesStartIndex, moviesEndIndex);
+    moviesContent += '</movies>';
+    doc = new Dom().parseFromString(moviesContent);
+    nodes = xpath.select("/movies/div/h3/a", doc);
     if (!nodes || nodes.length === 0) {
-        res.write("Cannot find movie entries in calendar feed");
+        res.write("Cannot find movie entries in movies coming out this week schedule");
         res.end();
         return;
     }
-    movieTitles = nodes.map(function (titleNode) {
-        return titleNode.nodeValue;
+    insiderMoviesUrls = nodes.map(function (node) {
+        return node.attributes[0].value; // href attribute
     });
-    nodes = select('//atom:feed/atom:entry/atom:summary/text()', doc);
-    firstShowingUrls = nodes.map(function (summaryNode) {
-        var summary = summaryNode.nodeValue,
-            urlStartIndex = summary.indexOf('http://'),
-            url = null;
-        if (urlStartIndex > 0) {
-            url = summary.substring(urlStartIndex);
-        }
-        return url;
+    movieTitles = nodes.map(function (node) {
+        return node.firstChild.nodeValue; // text value of the node
     });
-    movieRequests = movieTitles.map(function (movieTitle) {
-        return request(movieInfo.format(movieTitle));
+    insiderMoviesRequests = insiderMoviesUrls.map(function (url) {
+        return request(url);
     });
-    Promise.settle(movieRequests).then(function (movieRequestResults) {
-        return processMovieRequests(res, Promise, movies, Movie, movieTitles, firstShowingUrls, movieRequestResults);
+    Promise.settle(insiderMoviesRequests).then(function (insiderMovieRequestResults) {
+        var imdbIds = processInsiderMovieRequests(res, movieTitles, insiderMoviesUrls, insiderMovieRequestResults);
+        return imdbIds.map(function (imdbId) {
+            return imdbId === null ? Promise.reject({ handled: true }) : request(movieInfo.format(imdbId));
+        });
+    }).settle().then(function (movieRequestResults) {
+        return processMovieRequests(res, Promise, movies, Movie, movieTitles, movieRequestResults);
     }).settle().then(function (findMoviePromiseResults) {
         return saveMovies(res, Promise, movies, Movie, findMoviePromiseResults);
     }).settle().then(function (createMoviePromisesResults) {
@@ -199,9 +236,9 @@ function processMovieCalendar(res, request, Promise, calendarResponse, calendarB
                     res.write('Error while creating movie "{0}": {1}\r\n'.format(movies[index].title), rejectionReason);
                 }
             }
+            res.write("Finished processing movie calendar\r\n");
+            res.end();
         });
-        res.write("Finished processing movie calendar\r\n");
-        res.end();
     });
 }
 
@@ -209,10 +246,10 @@ exports.execute = function (req, res) {
     var Promise = require("bluebird"),
         request = Promise.promisify(require('request'));
     res.type('text');
-    requestMovieCalendar(request, req.query.startDate, req.query.endDate).spread(function (calendarResponse, calendarBody) {
-        processMovieCalendar(res, request, Promise, calendarResponse, calendarBody);
-    }).catch(function (calendarRequestError) {
-        res.write("Cannot retrieve movie calendar via GET request, got " + calendarRequestError);
+    request('http://www.movieinsider.com/movies/this-week/').spread(function (moviesResponse, moviesBody) {
+        processMovieCalendar(res, request, Promise, moviesResponse, moviesBody);
+    }).catch(function (moviesRequestError) {
+        res.write("Cannot retrieve movies via GET request, got " + moviesRequestError);
         res.end();
     });
 };
