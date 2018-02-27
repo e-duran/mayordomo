d2320fdd1099ead8a43405b46c8cd224588db2d4
Eric Duran <mediaboxeadl@outlook.com>
2018-02-27 13:35:07 +0000

    Rewrote all processors, feeds and APIs to use newer ES6 features (e.g. await). Added API for storing/retrieving log entries (when invoking processors as tasks). Added new UI app for viewing log entries based on Angular 5.

---
+++ api/logs.js
@@ -0,0 +1,19 @@
+'use strict';
+
+exports.register = function (app) {
+    app.get('/api/logCategories', async function (req, res) {
+        res.json(Object.keys(global.processorsMap));
+    });
+    
+    app.get('/api/logs', async function (req, res) {
+        var logsStore;
+        try {
+            logsStore = await global.getStore('logs');
+            var logs = await logsStore.find({ category: req.query.category }).sort('timestamp', -1).limit(50).toArray();
+            logsStore.client.close();
+            res.json(logs);
+        } catch (e) {
+            global.jsonApiError(res, logsStore);
+        }
+    });
+};
