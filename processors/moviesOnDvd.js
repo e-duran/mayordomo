574ab041c9fe0537c9ffbbcfdddb4ec39d0bb8ff
Eric Duran <mediaboxeadl@outlook.com>
2018-09-07 14:15:28 +0000

    Improved handling of errors in post-processing and added sending email on movie API error

--- processors/moviesOnDvd.js
+++ processors/moviesOnDvd.js
@@ -30,6 +30,22 @@ function updateMovie(movie, movieInfo, config) {
     return movie;
 }
 
+function sendMail(config, body, log) {
+    var Mailgun = require('mailgun-js');
+    var mailgun = new Mailgun({apiKey: config.mailgunApiKey, domain: config.mailgunDomain});
+    var mail = {
+        from: config.stockWatchListFrom,
+        to: config.stockWatchListTo,
+        subject: `Error in movies post-processing`,
+        html: body
+    };
+    mailgun.messages().send(mail, function (error, body) {
+        if (error) {
+            log('Error while sending mail', error);
+        }
+    });
+}
+
 exports.execute = async function (req, res) {
     res.type('text/plain; charset=utf-8');
     
@@ -48,19 +64,22 @@ exports.execute = async function (req, res) {
         movieStore = await global.getStore('movies');
         var movies = await movieStore.find({ needsReview: false, nextPostProcessingDate: moment().startOf('day').toDate() }).toArray();
         var action;
+        var lastError;
         for (var i = 0; i < movies.length; i++) {
+            var movieResponse = null;
+            var movieInfo = null;
             try {
                 var movie = movies[i];
                 var movieApiUrl = config.cinesiftUrl.format(encodeURI(movie.title), movie.year, movie.year);
-                var movieResponse = await axios.get(movieApiUrl);
-                var movieInfo = JSON.parse(movieResponse.data);
+                movieResponse = await axios.get(movieApiUrl);
+                movieInfo = JSON.parse(movieResponse.data);
                 if (movieInfo.length > 0) {
-                    if (movieInfo.length > 1) { log(`WARNING: Movie API returned multiple matches for "${movie.title}" (${movie.year})`); }
+                    if (movieInfo.length > 1) { log(`WARNING: Movie API returned ${movieInfo.length} matches for "${movie.title}" (${movie.year})`); }
                     movieInfo = movieInfo[0];
                     updateMovie(movie, movieInfo, config);
                     action = 'post-processed';
                 } else {
-                    action = 'not post-processed';
+                    action = 'not found in Movie API database';
                 }
                 movie.remainingPostProcessingTimes--;
                 movie.nextPostProcessingDate = movie.remainingPostProcessingTimes ? moment().add(1, 'months').startOf('day').toDate() : null;
@@ -70,13 +89,18 @@ exports.execute = async function (req, res) {
                 log(`Movie "${movie.title}" was ${action} with ${times} remaining rounds.`);
             } catch (e) {
                 log(`Error post-processing movie ${movies[i].title}`, e, true);
+                if (!movieResponse || !movieInfo) {
+                    lastError = movieResponse ? 'Movie API did not returned valid JSON response' : 'Error in GET request to movie API';
+                }
             }
+        }
+        if (lastError) {
+            sendMail(config, lastError, log);
         }
         movieStore.client.close();
         log(`Finished post-processing of ${movies.length} movies.`);
         res.end();
-    }
-     catch (e) {
+    } catch (e) {
         log('Exception', e);
         if (movieStore) {
             movieStore.client.close();
